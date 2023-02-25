0c1b2a897a9e833b61dbb56da778e88de3e97267
root <root@localhost.localdomain>
2023-02-25 16:24:57 +0000

    add package WYSIWYGTextEditor of Quilljs

---
+++ Pages/ProductPostBase.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Components;
+using mudblazor_augcenter.Models;
+using MudBlazor;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace mudblazor_augcenter.Pages
+{
+    public class ProductPostBase: ComponentBase
+    {
+
+        protected override async Task OnInitializedAsync()
+        {
+            try
+            {
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+    }
+}
